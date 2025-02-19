5d1549358f4eb12715b31623865aa5503372de55
maarkostosiic9 <mstosic@itpfp.com>
2025-02-19 09:31:35 +0100

    Agregado formulario de registro de usuarios

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 echo "Bienvenido al Gestor de Usuarios";
+
+echo '<form method="post" action="registro.php">
+        Nombre: <input type="text" name="nombre"><br>
+        Email: <input type="email" name="email"><br>
+        Contraseña: <input type="password" name="contrasena"><br>
+        <input type="submit" value="Registrar">
+      </form>';
 ?>
 
